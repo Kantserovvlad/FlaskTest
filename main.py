3b77cd10b51d965241df35d5b69845c2f1cbf17e
Kantserovvlad <kantserovvlad@yandex.ru>
2022-04-12 17:45:04 +0300

    Изменение кнопок с авторизацией

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+
 if __name__ == '__main__':
     db_session.global_init("db/diary.db")
-    app.run(host='0.0.0.0', port=5000)
-    # serve(app, host='0.0.0.0', port=5000)
+    # app.run(host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0', port=5000)

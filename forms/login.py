ddadecd8fb9c8bf7d4ca8766050f9a5fbd29b714
Kantserovvlad <kantserovvlad@yandex.ru>
2022-04-24 17:09:36 +0300

    Добавил возможность указать школу и класс

--- forms/login.py
+++ forms/login.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, BooleanField, SubmitField
+from wtforms import PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.fields import EmailField
 from wtforms.validators import DataRequired, Email
 
@@ -9,3 +9,14 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+
+class SetSchool(FlaskForm):
+    school_id = SelectField('Выберите школу')
+    submit = SubmitField('Сохранить и указать класс')
+
+
+class SetClass(FlaskForm):
+    class_n_id = SelectField('Выберите класс')
+    submit = SubmitField('Сохранить')
+
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, url_for, redirect
+from flask_restful import abort
 from waitress import serve
 
 from data.class_n import Classes
-from forms.login import LoginForm
+from forms.login import LoginForm, SetSchool, SetClass
 from data import db_session
 from data.users import User
 from data.schools import School
@@ -29,47 +30,58 @@ def index():
 @app.route('/personal')
 def personal_account():
     if current_user.is_authenticated:
-        return render_template('personal.html')
+        db_sess = db_session.create_session()
+        school = db_sess.query(School).filter(current_user.school_id == School.id).first()
+        class_n = db_sess.query(Classes).filter(current_user.class_n_id == Classes.id).first()
+        if school is not None:
+            school = school.name
+        if class_n is not None:
+            class_n = class_n.name
+        return render_template('personal.html', school=school, class_n=class_n)
     return redirect('/')
 
 
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('registration.html', form=form,
-                                   message="Пароли не совпадают")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('registration.html', form=form,
-                                   message="Такой пользователь уже есть")
-        user = User(
-            name=form.name.data,
-            surname=form.surname.data,
-            email=form.email.data,
-        )
+    if not current_user.is_authenticated:
+        form = RegisterForm()
+        if form.validate_on_submit():
+            if form.password.data != form.password_again.data:
+                return render_template('registration.html', form=form,
+                                       message="Пароли не совпадают")
+            db_sess = db_session.create_session()
+            if db_sess.query(User).filter(User.email == form.email.data).first():
+                return render_template('registration.html', form=form,
+                                       message="Такой пользователь уже есть")
+            user = User(
+                name=form.name.data,
+                surname=form.surname.data,
+                email=form.email.data,
+            )
 
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('registration.html', form=form)
+            user.set_password(form.password.data)
+            db_sess.add(user)
+            db_sess.commit()
+            return redirect('/login')
+        return render_template('registration.html', form=form)
+    return redirect('/personal')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('login.html', form=form)
+    if not current_user.is_authenticated:
+        form = LoginForm()
+        if form.validate_on_submit():
+            db_sess = db_session.create_session()
+            user = db_sess.query(User).filter(User.email == form.email.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user, remember=form.remember_me.data)
+                return redirect("/")
+            return render_template('login.html',
+                                   message="Неправильный логин или пароль",
+                                   form=form)
+        return render_template('login.html', form=form)
+    return redirect('/personal')
 
 
 @app.route('/logout')
@@ -77,6 +89,49 @@ def login():
 def logout():
     logout_user()
     return redirect("/")
+
+
+@app.route('/set/school', methods=['GET', 'POST'])
+def set_school():
+    if current_user.is_authenticated:
+        form = SetSchool()
+        title = 'Установить школу'
+        db_sess = db_session.create_session()
+        schools = [(i.id, i.name) for i in db_sess.query(School).all()]
+        form.school_id.choices = schools
+        if form.validate_on_submit():
+            user = db_sess.query(User).filter(User.id == current_user.id).first()
+            if user:
+                user.school_id = form.school_id.data
+                db_sess.commit()
+                return redirect('/set/class')
+            else:
+                abort(404)
+        return render_template('set_school_or_class.html', form=form, title=title)
+    return redirect('/login')
+
+
+@app.route('/set/class', methods=['GET', 'POST'])
+def set_class():
+    if current_user.is_authenticated:
+        if current_user.school_id:
+            form = SetClass()
+            title = 'Установить класс'
+            db_sess = db_session.create_session()
+            classes = [(i.id, i.name) for i in
+                       db_sess.query(Classes).filter(Classes.school_id == current_user.school_id).all()]
+            form.class_n_id.choices = classes
+            if form.validate_on_submit():
+                user = db_sess.query(User).filter(User.id == current_user.id).first()
+                if user:
+                    user.class_n_id = form.class_n_id.data
+                    db_sess.commit()
+                    return redirect('/personal')
+                else:
+                    abort(404)
+            return render_template('set_school_or_class.html', form=form, title=title)
+        return redirect('/set/school')
+    return redirect('/login')
 
 
 if __name__ == '__main__':
